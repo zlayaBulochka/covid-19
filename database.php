af165b3def3af9cc84576cbdfddc202e7ffd6393
Roman <romires2000@gmail.com>
2020-06-10 15:31:45 +0300

    fix paths

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysqli_connect('localhost:3308', 'server', 'server', 'pig');
+$link = mysqli_connect('localhost', 'server', 'server', 'pig');
     if (!$link) {
           die("Connection failed: " . mysqli_connect_error());
     }
